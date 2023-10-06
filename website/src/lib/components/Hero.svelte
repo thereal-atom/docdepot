482123ecbcdea0117b270b42d667244c074b0f3e
thereal-atom <oscarfal2006@gmail.com>
2023-10-06 22:05:06 +0100

    landing page wow

---
+++ website/src/lib/components/Hero.svelte
@@ -0,0 +1,99 @@
+<script>
+    $: tab = "markdown";
+</script>
+
+<div class="flex flex-col w-[640px] mt-8 border border-white border-opacity-10 bg-primary rounded-md transition-all max-md:w-[500px] max-sm:w-[380px] max-[400px]:w-[300px]">
+    <div class="flex flex-row w-full ">
+        <button
+            class="w-1/2 py-1 text-[8px] rounded-md m-2 transition-all hover:cursor-pointer focus:outline-none {tab === "markdown" ? "bg-white bg-opacity-5" : ""}"
+            on:click={() => tab = "markdown"}
+        >
+            Local Markdown
+        </button>
+        <button
+            class="w-1/2 py-1 text-[8px] rounded-md m-2 transition-all hover:cursor-pointer focus:outline-none {tab === "preview" ? "bg-white bg-opacity-5" : ""}"
+            on:click={() => tab = "preview"}
+        >
+            Published Markdown
+        </button>
+    </div>
+    {#if tab === "markdown"}
+        <div class="flex flex-col w-full h-[360px] overflow-y-hidden bg-primary py-4 px-16 rounded-b-md max-md:h-[280px] max-sm:h-[200px] max-sm:px-8 max-[400px]:h-[140px] max-[400px]:px-4">
+            <p class="h1"><span class="hash">#</span> a guide to doc depot</p>
+            <p class="p">DocDeopt is a very simple solution to upload your markdown files</p>
+            <p class="p">In just a few steps, you can make your knowledge available to the whole world via Markdown and DocDepot.</p>
+            <p class="h2"><span class="hash">#</span><span class="hash">#</span> Step 1. Go to the upload page</p>
+            <p class="p">Go to DocDepot's upload page at: <span class="syntax">[</span>upload<span class="syntax">](</span><span class="a">https://docdepot.com/upload</span><span class="syntax">)</span></p>
+            <p class="h2"><span class="hash">#</span><span class="hash">#</span> Step 2. Paste your markdown</p>
+            <p class="p">From here, the page will show you the step by step on how to upload your file.</p>
+            <p class="p">You will see two main boxes towards the top of the page. Paste your markdown into the box on the left and it will show you a preview in the box in the right. This is example how it will look like when it's uploaded.</p>
+            <div class="h2"><span class="hash">#</span><span class="hash">#</span> Step 3. Name your document</div>
+            <p class="p">The next input box will ask you for a name for your document. This is the name by which people can access your document.</p>
+            <p class="p">If you name your document <span class="syntax">`</span>guide<span class="syntax">`</span>, it will be found under the following URL: <span class="syntax">[</span>https://docdepot.com/guide<span class="syntax">](</span><span class="a">https://docdepot.com/guide</span><span class="syntax">)</span></p>
+            <p class="p">Everyone will be able to view it but I am working on features which will allow you to restrict viewership.</p>
+            <div class="h2"><span class="hash">#</span><span class="hash">#</span> Step 4. Protect your document</div>
+            <p class="p">On the home page I state that you don't need to login in order to publish your markdown. This is true but also not very secure so, if you are not logged in, you need to provide a password.</p>
+            <p class="p">This will be used if you want to edit/remove your document and when you want to add the document to your account. Once you add the document to your account, you can edit/remove it without having to enter the password.</p>
+            <div class="h2"><span class="hash">#</span><span class="hash">#</span> Step 5. Upload your document</div>
+            <p class="p">There will be a button at the bottom of the page which you should press. Your document is now published.</p>
+            <p class="p">As mentioned earlier, it will be found at <span class="syntax">[</span> https://docdepot.com/view/{"<"}name> <span class="syntax">](</span><span class="a">https://docdepot.com/guide</span><span class="syntax">)</span>, and it will give you the URL when you upload.</p>
+            <p class="p"><span class="syntax">**</span>And that's it.<span class="syntax">**</span> This is supposed to be a very simple tool at this point, and will stay this way for a while.</p>
+        </div>
+    {:else}
+        <div class="flex flex-col w-full h-[360px] overflow-y-hidden bg-primary py-4 px-16 rounded-b-md max-md:h-[280px] max-sm:h-[200px] max-sm:px-8 max-[400px]:h-[140px] max-[400px]:px-4">
+            <p class="h1">a guide to doc depot</p>
+            <p class="p">DocDeopt is a very simple solution to upload your markdown files</p>
+            <p class="p">In just a few steps, you can make your knowledge available to the whole world via Markdown and DocDepot.</p>
+            <p class="h2">Step 1. Go to the upload page</p>
+            <p class="p">Go to DocDepot's upload page at: <span class="a">upload</span></p>
+            <p class="h2">Step 2. Paste your markdown</p>
+            <p class="p">From here, the page will show you the step by step on how to upload your file.</p>
+            <p class="p">You will see two main boxes towards the top of the page. Paste your markdown into the box on the left and it will show you a preview in the box in the right. This is example how it will look like when it's uploaded.</p>
+            <div class="h2">Step 3. Name your document</div>
+            <p class="p">The next input box will ask you for a name for your document. This is the name by which people can access your document.</p>
+            <p class="p">If you name your document <span class="code">guide</span>, it will be found under the following URL: <span class="a">https://docdepot.com/guide</span></p>
+            <p class="p">Everyone will be able to view it but I am working on features which will allow you to restrict viewership.</p>
+            <div class="h2">Step 4. Protect your document</div>
+            <p class="p">On the home page I state that you don't need to login in order to publish your markdown. This is true but also not very secure so, if you are not logged in, you need to provide a password.</p>
+            <p class="p">This will be used if you want to edit/remove your document and when you want to add the document to your account. Once you add the document to your account, you can edit/remove it without having to enter the password.</p>
+            <div class="h2">Step 5. Upload your document</div>
+            <p class="p">There will be a button at the bottom of the page which you should press. Your document is now published.</p>
+            <p class="p">As mentioned earlier, it will be found at <span class="a">https://docdepot.com/view/{"<"}name</span>, and it will give you the URL when you upload.</p>
+            <p class="p"><span class="bold">And that's it.</span> This is supposed to be a very simple tool at this point, and will stay this way for a while.</p>
+        </div>
+    {/if}
+</div>
+
+<style lang="postcss">
+    .h1 {
+        @apply text-[10px] font-bold my-2 max-sm:text-[8px] max-sm:my-1;
+    }
+
+    .h2 {
+        @apply text-[8px] font-bold my-2 max-sm:text-[6px] max-sm:my-1;
+    }
+
+    .p {
+        @apply text-[6px] font-medium my-0.5 max-sm:text-[4px] max-sm:my-0.5;
+    }
+
+    .hash {
+        @apply opacity-60;
+    }
+
+    .a {
+        @apply text-indigo-500;
+    }
+
+    .syntax {
+        @apply text-gray-500;
+    }
+
+    .code {
+        @apply bg-gray-900 px-[2px] py-[1px] rounded-[2px] text-red-500;
+    }
+
+    .bold {
+        @apply font-bold;
+    }
+</style>
--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { enhance } from "$app/forms";
+	import Hero from "$lib/components/Hero.svelte";
 	import { addToast } from "$lib/stores/toasts";
 
     $: loading = false;
@@ -44,31 +45,34 @@
         };
     };
 </script>
-<div class="flex flex-col min-h-screen">
-    <div class="flex flex-col items-center min-h-screen max-sm:min-h-fit bg-gradient-to-br from-[#7353BA] to-[#2F195F]">
-        <div class="flex flex-col items-center py-16">
-            <h1 class="text-5xl font-black max-lg:text-4xl max-sm:text-2xl">Publish Your Knowledge</h1>
-            <p class="mt-4 text-gray-300 text-xl font-bold text-center max-lg:text-base max-sm:text-sm max-sm:px-4">Upload your markdown files to make them available for everyone to see. It literally takes one step.</p>
-            <div class="font-bold max-lg:text-sm max-sm:text-xs">(you don't even need to login)</div>
-            <a
-                href="/upload"
-                class="mt-4 px-4 py-2 bg-primary font-bold rounded-md max-sm:text-sm"
-            >
-                Upload Markdown
-            </a>
-            <img
-                class="w-3/4 mt-16 rounded-md max-xl:w-full max-xl:px-8 max-md:hidden"
-                src="/hero-image.webp"
-                alt="hero"
-            />
-            <img
-                class="w-full mt-12 px-8 rounded-md md:hidden max-sm:mt-24"
-                src="/hero-image-small.webp"
-                alt="hero"
-            />
+
+<div class="flex flex-col">
+    <div class="main relative flex flex-col items-center justify-center w-full h-screen max-xl:h-fit max-xl:py-32 max-lg:py-24 max-md:h-5/6 max-md:px-8">
+        <div class="z-10 flex flex-col items-center">
+            <h1 class="text-5xl font-black text-center max-lg:text-4xl max-md:text-3xl max-[400px]:text-xl">Share your knowledge</h1>
+            <p class="mt-4 text-lg font-semibold opacity-60 text-center max-md:text-base">Upload your markdown files to make them available for everyone to see.</p>
+            <Hero />
+        </div>
+
+        <!-- TODO: REFACTOR BRO WTF IS THIS. but it works so fine for now. -->
+        <div class="max-xl:opacity-30">
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-medium left-20 max-lg:left-12 top-[100px] rotate-[-15deg]"><span class="opacity-20">1.</span> an ordered list</p>
+            <p class="absolute opacity-80 text-lg max-lg:text-sm font-medium left-48 max-lg:left-16 top-[200px] rotate-[15deg] py-8 pl-8 pr-12 rounded-md border-l-4 border-white border-opacity-20 bg-white bg-opacity-[5%]"><span class="opacity-20">{">"}</span> block quotes :0</p>
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-medium left-20 max-lg:left-12 top-[350px]">paragraph</p>
+            <p class="absolute opacity-60 text-3xl max-lg:text-2xl font-bold left-32 max-lg:left-8 top-[450px] -rotate-[15deg]"><span class="opacity-20">#</span> heading 1</p>
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-bold left-28 max-lg:left-8 top-[600px] max-lg:top-[550px]"><span class="opacity-20">[</span><span class="text-indigo-500">link</span><span class="opacity-20">](</span>docdepot.com<span class="opacity-20">)</span></p>
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-bold left-20 max-lg:left-12 top-[700px] max-lg:top-[620px] rotate-[15deg]"><span class="opacity-20">*</span>italic text<span class="opacity-20">*</span></p>
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-bold left-48 top-[800px] max-lg:top-[640px] -rotate-[15deg] max-lg:rotate-0"><span class="opacity-20">-</span> another list</p>
+    
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-bold right-20 max-lg:right-12 top-[100px] rotate-[15deg]"><span class="opacity-20">**</span>bold text<span class="opacity-20">**</span></p>
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-bold right-32 max-lg:right-8 top-[250px] -rotate-[15deg]"><span class="opacity-20">![</span><span class="text-indigo-500">image</span><span class="opacity-20">](</span>imgur.com/wow<span class="opacity-20">)</span></p>
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-bold right-28 max-lg:right-4 top-[350px] p-6 bg-white bg-opacity-[5%] rounded-md"><span class="opacity-20">```</span><br />// codeblocks<br /><span class="opacity-20">```</span></p>
+            <p class="absolute opacity-60 text-2xl max-lg:text-xl font-bold right-56 max-lg:right-12 top-[580px] max-lg:top-[500px] rotate-[15deg]"><span class="opacity-20">##</span> heading 2</p>
+            <p class="absolute opacity-60 text-lg max-lg:text-sm font-bold right-80 max-lg:right-12 top-[700px] max-lg:top-[600px]"><span class="opacity-20">`</span>code<span class="opacity-20">`</span></p>
+            <p class="absolute opacity-60 text-xl max-lg:text-lg font-bold right-36 top-[820px] max-lg:top-[630px] -rotate-[15deg] max-lg:rotate-0"><span class="opacity-20">###</span> heading 3</p>
         </div>
     </div>
-    <div class="flex flex-col items-center my-64 max-md:my-32 max-sm:my-16 max-sm:px-4">
+    <div class="z-10 bg-primary flex flex-col items-center border-y border-white border-opacity-5 py-64 max-md:py-32 max-sm:py-16 max-sm:px-4">
         <p class="text-2xl font-bold text-center max-sm:text-sm">Join the email newsletter to stay tuned for updates.</p>
         <form
             class="flex flex-row max-sm:flex-col max-sm:w-full"
@@ -90,7 +94,17 @@
             </button>
         </form>
     </div>
-    <div class="flex flex-col items-center w-full my-32">
-        <p class="text-3xl font-bold">If you would like beta access, reach out on <a class="text-indigo-500" href="https://twitter.com/oscarfalll">twitter</a>.</p>
+    <div class="sub flex flex-col items-center w-full py-64">
+        <p class="px-8 text-3xl font-bold text-center">If you would like beta access, reach out on <a class="text-indigo-500" href="https://twitter.com/oscarfalll">twitter</a>.</p>
     </div>
 </div>
+
+<style lang="postcss">
+    .main {
+        background: radial-gradient(circle, rgba(38,10,107,1) 10%, rgba(1,9,18,1) 70%, rgba(1,9,18,1) 100%);
+    }
+
+    .sub {
+        background: radial-gradient(circle, rgba(38,10,107,1) 5%, rgba(1,9,18,1) 40%, rgba(1,9,18,1) 100%);
+    }
+</style>
